1d2f69d55d0ecb806528e45aecaca5ed65068e42
henrikpoa <henrik_poa@hotmail.com>
2024-03-15 09:39:37 -0300

    Fiz validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.devsuperior.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -21,7 +22,8 @@ public class ProductDTO {
     
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
-    private String description;    
+    private String description;
+    @NotNull(message = "Campo requirido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;    
     private String imgUrl;
